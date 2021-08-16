f594663d3a86bfdcc427a762c187e5d86f92055a
Agnibho1678 <73741624+Agnibho1678@users.noreply.github.com>
2021-08-16 16:20:21 +0530

    Add files via upload

--- z-score.py
+++ z-score.py
@@ -28,6 +28,7 @@ mean = statistics.mean(mean_list)
 print("mean of sampling distribution:- ",mean)
 print("Standard deviation of sampling distribution:- ", std_deviation)
 
+
 first_std_deviation_start, first_std_deviation_end = mean-std_deviation, mean+std_deviation
 second_std_deviation_start, second_std_deviation_end = mean-(2*std_deviation), mean+(2*std_deviation)
 third_std_deviation_start, third_std_deviation_end = mean-(3*std_deviation), mean+(3*std_deviation)
@@ -44,6 +45,7 @@ fig.add_trace(go.Scatter(x=[first_std_deviation_end, first_std_deviation_end], y
 fig.add_trace(go.Scatter(x=[second_std_deviation_end, second_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 2 END"))
 fig.add_trace(go.Scatter(x=[third_std_deviation_end, third_std_deviation_end], y=[0, 0.17], mode="lines", name="STANDARD DEVIATION 3 END"))
 fig.show()
+
 
 
 df = pd.read_csv("School_2_Sample.csv")
